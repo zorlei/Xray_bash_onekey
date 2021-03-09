8f3bb8269b64e1491e791fe0560e5c4b783d2449
paniy <louisowen@me.com>
2021-03-09 14:31:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ Error="${Red}[错误]${Font}"
 Warning="${Red}[警告]${Font}"
 
 # 版本
-shell_version="1.4.0.0"
+shell_version="1.4.0.1"
 shell_mode="None"
 version_cmp="/tmp/version_cmp.tmp"
 xray_conf_dir="/usr/local/etc/xray"
