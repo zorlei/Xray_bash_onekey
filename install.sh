d6671349eba0df0274d7fb8977f8974f0e8084a1
paniy <louisowen@me.com>
2021-03-24 09:49:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ Error="${Red}[错误]${Font}"
 Warning="${Red}[警告]${Font}"
 
 # 版本
-shell_version="1.4.4.0"
+shell_version="1.4.4.1"
 shell_mode="None"
 shell_mode_show="未安装"
 version_cmp="/tmp/version_cmp.tmp"
