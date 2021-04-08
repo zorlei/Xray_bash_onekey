a8b94e26407b307e2d53f3eb2f5c576571501081
paniy <louisowen@me.com>
2021-04-08 10:55:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -783,7 +783,6 @@ nginx_conf_add() {
         ssl_ciphers           TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-CCM-8-SHA256:TLS13-AES-128-CCM-SHA256:EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
         server_name           serveraddr.com;
         index index.html index.htm;
-        #root  /home/wwwroot/3DCEList;
         root /400.html;
         error_page 400 https://www.idleleo.com/helloworld;
         # Config for 0-RTT in TLSv1.3
@@ -811,6 +810,7 @@ nginx_conf_add() {
         # Config for 0-RTT in TLSv1.3
             proxy_set_header Early-Data \$ssl_early_data;
         }
+        
         locatioc
         {
             returc
